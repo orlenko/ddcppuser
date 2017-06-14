f3a9641656e012efe80ee5dc63942d0a86f28c7e
Vlad Orlenko <orlenko@gmail.com>
2017-06-14 13:07:04 -0700

    Debugging npm install

---
+++ main.cpp
@@ -0,0 +1,12 @@
+
+
+#include <iostream>
+
+#include "cpp_common/sttools.hpp"
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+    cout << dd::i_will_break_you("foo", 1, 2) << endl;
+    return 0;
+}
