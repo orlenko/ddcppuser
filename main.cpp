21ef6c3d0601a5dac5a5f09dcc7c284266893838
Vlad Orlenko <orlenko@gmail.com>
2017-06-14 14:37:43 -0700

    nice failing build with dependencies

--- main.cpp
+++ main.cpp
@@ -2,11 +2,13 @@
 
 #include <iostream>
 
-#include "cpp_common/src/strtools.hpp"
+#include "node_modules/cpp_common/src/strtools.hpp"
+#include "node_modules/ddcppuserlib/src/dummy.hpp"
 
 using namespace std;
 
 int main(int argc, char *argv[]) {
     cout << dd::i_will_break_you("foo", 1, 2) << endl;
+    dummy_lib_func();
     return 0;
 }
